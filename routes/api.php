4942a55deb0f9b83c87797210ab55921b63cc499
João Paulo Leite Costa <joaopaulotds@gmail.com>
2025-03-24 23:00:51 -0300

    Route clubs apiresource configured

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\ClubController;
 use App\Services\ApiResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/status', function() {
     return ApiResponse::success('API is running');
-}); //->middleware('auth:sanctum');
+}); 
+
+Route::apiResource('clubs', ClubController::class);
